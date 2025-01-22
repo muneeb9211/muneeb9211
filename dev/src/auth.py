c7c764b7c939c63ff3f2474eebfd72f0abf6f98f
muneeb9211 <muneebahmad9211@gmail.com>
2025-01-22 18:41:15 +0500

    Fix authentication bug in login flow

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -4,3 +4,4 @@
 # 2025-01-13T15:24:51  Fix authentication bug  (rev 19)
 # 2025-01-14T17:12:28  Fix authentication bug  (rev 22)
 # 2025-01-16T13:32:47  Handle expired tokens on refresh  (rev 25)
+# 2025-01-22T18:41:15  Fix authentication bug in login flow  (rev 30)
