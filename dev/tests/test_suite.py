67c3b567f3c0283baf877a692228a31537f4d578
muneeb9211 <muneebahmad9211@gmail.com>
2025-01-27 20:06:19 +0500

    Add unit tests

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -2,3 +2,4 @@
 # 2025-01-08T19:47:12  Add edge-case tests for parser  (rev 14)
 # 2025-01-08T21:51:39  Add unit tests  (rev 15)
 # 2025-01-14T12:14:27  Add edge-case tests for parser  (rev 21)
+# 2025-01-27T20:06:19  Add unit tests  (rev 34)
