655ed00dc4fd45efb74a086ac311b0988658c612
muneeb9211 <muneebahmad9211@gmail.com>
2025-02-04 14:25:08 +0500

    Fix layout shift on load

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -1,3 +1,4 @@
 """UI components."""
 # 2025-01-07T13:31:07  Polish loading states  (rev 9)
 # 2025-01-11T19:18:47  Debounce search input  (rev 18)
+# 2025-02-04T14:25:08  Fix layout shift on load  (rev 44)
