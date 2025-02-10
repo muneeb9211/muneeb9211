896f38bab42f7a4953a5c3d098d88cc09f4b9250
muneeb9211 <muneebahmad9211@gmail.com>
2025-02-10 16:43:47 +0500

    Fix authentication bug

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -7,3 +7,4 @@
 # 2025-01-22T18:41:15  Fix authentication bug in login flow  (rev 30)
 # 2025-01-24T10:14:39  Fix session timeout handling  (rev 32)
 # 2025-02-05T15:49:50  Fix authentication bug in login flow  (rev 47)
+# 2025-02-10T16:43:47  Fix authentication bug  (rev 50)
