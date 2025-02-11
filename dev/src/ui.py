19fa1f44234fbe1e4a6853f8fc4b735e0879a4be
muneeb9211 <muneebahmad9211@gmail.com>
2025-02-11 12:49:56 +0500

    Debounce search input

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -2,3 +2,4 @@
 # 2025-01-07T13:31:07  Polish loading states  (rev 9)
 # 2025-01-11T19:18:47  Debounce search input  (rev 18)
 # 2025-02-04T14:25:08  Fix layout shift on load  (rev 44)
+# 2025-02-11T12:49:56  Debounce search input  (rev 54)
