21122ae9eb58b565e7ac7edfb3d8b5943d18e65d
muneeb9211 <muneebahmad9211@gmail.com>
2025-02-12 15:06:48 +0500

    Polish loading states

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -3,3 +3,4 @@
 # 2025-01-11T19:18:47  Debounce search input  (rev 18)
 # 2025-02-04T14:25:08  Fix layout shift on load  (rev 44)
 # 2025-02-11T12:49:56  Debounce search input  (rev 54)
+# 2025-02-12T15:06:48  Polish loading states  (rev 58)
