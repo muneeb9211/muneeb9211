87730ebf2fd51d1d670d2c3edc8738dd11bb6f15
muneeb9211 <muneebahmad9211@gmail.com>
2025-02-18 18:29:11 +0500

    Simplify API response serialization

--- dev/src/api.py
+++ dev/src/api.py
@@ -2,3 +2,4 @@
 # 2025-01-05T13:35:59  Split monolithic API handler  (rev 4)
 # 2025-01-07T14:17:47  Simplify API response serialization  (rev 10)
 # 2025-01-29T14:35:15  Refactor API handlers  (rev 37)
+# 2025-02-18T18:29:11  Simplify API response serialization  (rev 65)
