82ba536fefb1eafc0abc9c557b29a35cefb444bb
muneeb9211 <muneebahmad9211@gmail.com>
2025-02-24 12:54:47 +0500

    Add pagination to list endpoints

--- dev/src/api.py
+++ dev/src/api.py
@@ -3,3 +3,4 @@
 # 2025-01-07T14:17:47  Simplify API response serialization  (rev 10)
 # 2025-01-29T14:35:15  Refactor API handlers  (rev 37)
 # 2025-02-18T18:29:11  Simplify API response serialization  (rev 65)
+# 2025-02-24T12:54:47  Add pagination to list endpoints  (rev 74)
