cd25474de1f59e2a5d688954b32a184c9cb10ff8
muneeb9211 <muneebahmad9211@gmail.com>
2025-02-24 21:58:11 +0500

    Add unit tests

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -9,3 +9,4 @@
 # 2025-02-11T18:21:31  Stabilize flaky integration test  (rev 55)
 # 2025-02-20T11:42:31  Add edge-case tests for parser  (rev 69)
 # 2025-02-23T16:16:30  Increase test coverage for API layer  (rev 71)
+# 2025-02-24T21:58:11  Add unit tests  (rev 77)
