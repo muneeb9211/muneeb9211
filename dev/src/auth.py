3ddb66b99c16f6a8c47294edcca8130a52038664
muneeb9211 <muneebahmad9211@gmail.com>
2025-03-07 19:32:26 +0500

    Fix authentication bug

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -12,3 +12,4 @@
 # 2025-02-18T21:01:31  Patch OAuth callback edge case  (rev 66)
 # 2025-02-23T20:04:09  Handle expired tokens on refresh  (rev 73)
 # 2025-02-27T16:02:36  Fix authentication bug  (rev 80)
+# 2025-03-07T19:32:26  Fix authentication bug  (rev 93)
