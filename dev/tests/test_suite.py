c879f5c01155a6ab73dcb1120d8e4757dd0e9cff
muneeb9211 <muneebahmad9211@gmail.com>
2025-03-08 16:41:59 +0500

    Stabilize flaky integration test

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -12,3 +12,4 @@
 # 2025-02-24T21:58:11  Add unit tests  (rev 77)
 # 2025-02-28T19:23:53  Add regression test for auth flow  (rev 81)
 # 2025-03-04T21:02:16  Add regression test for auth flow  (rev 87)
+# 2025-03-08T16:41:59  Stabilize flaky integration test  (rev 96)
