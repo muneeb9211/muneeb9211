809b5b23f876b46a653ef82c584a0d86f85fa076
muneeb9211 <muneebahmad9211@gmail.com>
2025-05-03 11:00:12 +0500

    Cache frequent read queries

--- dev/src/db.py
+++ dev/src/db.py
@@ -15,3 +15,4 @@
 # 2025-04-12T11:35:56  Cache frequent read queries  (rev 129)
 # 2025-04-16T15:32:28  Reduce N+1 queries in reports  (rev 136)
 # 2025-04-19T12:36:55  Add index to speed up lookups  (rev 139)
+# 2025-05-03T11:00:12  Cache frequent read queries  (rev 162)
