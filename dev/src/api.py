8697a9e4a465d7a207116e3464ee322c094875ce
muneeb9211 <muneebahmad9211@gmail.com>
2025-05-06 13:32:44 +0500

    Refactor API handlers

--- dev/src/api.py
+++ dev/src/api.py
@@ -8,3 +8,4 @@
 # 2025-03-12T13:01:56  Refactor API handlers  (rev 99)
 # 2025-03-24T16:17:21  Refactor API request routing  (rev 111)
 # 2025-04-12T14:24:59  Add pagination to list endpoints  (rev 130)
+# 2025-05-06T13:32:44  Refactor API handlers  (rev 164)
