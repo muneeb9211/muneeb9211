8653a5346b97a26193d8eab1e59501e656799e76
muneeb9211 <muneebahmad9211@gmail.com>
2025-06-03 12:02:14 +0500

    Improve UI responsiveness

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -26,3 +26,4 @@
 # 2025-05-10T19:54:56  Polish loading states  (rev 173)
 # 2025-05-20T12:12:55  Improve UI responsiveness  (rev 178)
 # 2025-05-27T18:46:32  Improve mobile responsiveness  (rev 189)
+# 2025-06-03T12:02:14  Improve UI responsiveness  (rev 195)
