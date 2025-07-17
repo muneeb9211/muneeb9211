fc8b0fe2e59bddeb0ce362c0f23d46fae4b7855d
muneeb9211 <muneebahmad9211@gmail.com>
2025-07-17 15:13:16 +0500

    Split monolithic API handler

--- dev/src/api.py
+++ dev/src/api.py
@@ -20,3 +20,4 @@
 # 2025-06-30T13:19:30  Split monolithic API handler  (rev 231)
 # 2025-07-14T14:39:44  Split monolithic API handler  (rev 255)
 # 2025-07-15T11:42:45  Add pagination to list endpoints  (rev 258)
+# 2025-07-17T15:13:16  Split monolithic API handler  (rev 262)
