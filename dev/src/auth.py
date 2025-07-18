b91cf0aac50e842b71dec861aecfbc7405d267fe
muneeb9211 <muneebahmad9211@gmail.com>
2025-07-18 17:17:40 +0500

    Fix authentication bug

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -27,3 +27,4 @@
 # 2025-06-24T09:30:24  Handle expired tokens on refresh  (rev 222)
 # 2025-07-02T20:48:15  Fix authentication bug  (rev 238)
 # 2025-07-03T13:04:39  Handle expired tokens on refresh  (rev 240)
+# 2025-07-18T17:17:40  Fix authentication bug  (rev 265)
