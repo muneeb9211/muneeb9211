fb41eec3e97873e7498b795d33f236b8b17c9a7d
muneeb9211 <muneebahmad9211@gmail.com>
2025-08-20 15:11:15 +0500

    Rename variables for clarity

--- dev/src/utils.py
+++ dev/src/utils.py
@@ -41,3 +41,4 @@
 # 2025-08-05T12:39:57  Rename variables for clarity  (rev 291)
 # 2025-08-18T14:34:39  Cleanup unused code  (rev 304)
 # 2025-08-19T19:13:44  Drop deprecated helpers  (rev 306)
+# 2025-08-20T15:11:15  Rename variables for clarity  (rev 308)
