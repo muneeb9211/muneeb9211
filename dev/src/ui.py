01ce6ae03dead41361b34b983a9fa3a38e6a65e1
muneeb9211 <muneebahmad9211@gmail.com>
2025-08-30 20:01:22 +0500

    Debounce search input

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -46,3 +46,4 @@
 # 2025-08-26T18:55:06  Debounce search input  (rev 314)
 # 2025-08-28T10:58:05  Polish loading states  (rev 317)
 # 2025-08-28T18:14:23  Polish loading states  (rev 318)
+# 2025-08-30T20:01:22  Debounce search input  (rev 320)
