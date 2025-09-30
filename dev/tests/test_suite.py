9b6927ae8b3f81a4033bf9df30413f6563b06643
muneeb9211 <muneebahmad9211@gmail.com>
2025-09-30 11:13:02 +0500

    Stabilize flaky integration test

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -45,3 +45,4 @@
 # 2025-09-20T14:35:12  Increase test coverage for API layer  (rev 346)
 # 2025-09-24T10:31:23  Stabilize flaky integration test  (rev 349)
 # 2025-09-29T21:13:09  Add edge-case tests for parser  (rev 358)
+# 2025-09-30T11:13:02  Stabilize flaky integration test  (rev 359)
