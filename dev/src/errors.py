bad859a95d67025756d90cc3835e766532199f48
muneeb9211 <muneebahmad9211@gmail.com>
2025-10-01 10:22:11 +0500

    Add graceful degradation on failure

--- dev/src/errors.py
+++ dev/src/errors.py
@@ -51,3 +51,4 @@
 # 2025-09-19T20:05:15  Return structured error responses  (rev 344)
 # 2025-09-25T10:06:40  Return structured error responses  (rev 352)
 # 2025-09-29T15:03:10  Return structured error responses  (rev 357)
+# 2025-10-01T10:22:11  Add graceful degradation on failure  (rev 360)
