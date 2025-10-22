7434108d38a5af94aaf1075335b2f4c1a2fe9abd
muneeb9211 <muneebahmad9211@gmail.com>
2025-10-22 19:05:24 +0500

    Split monolithic API handler

--- dev/src/api.py
+++ dev/src/api.py
@@ -43,3 +43,4 @@
 # 2025-10-03T18:13:22  Refactor API request routing  (rev 364)
 # 2025-10-06T20:30:01  Refactor API handlers  (rev 368)
 # 2025-10-16T12:44:01  Split monolithic API handler  (rev 379)
+# 2025-10-22T19:05:24  Split monolithic API handler  (rev 388)
