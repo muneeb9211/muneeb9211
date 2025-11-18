c45cf8d1bda019e6fd75ad66230094f682d76770
muneeb9211 <muneebahmad9211@gmail.com>
2025-11-18 21:48:03 +0500

    Reduce N+1 queries in reports

--- dev/src/db.py
+++ dev/src/db.py
@@ -50,3 +50,4 @@
 # 2025-11-04T15:09:19  Cache frequent read queries  (rev 401)
 # 2025-11-13T18:23:00  Batch database writes  (rev 408)
 # 2025-11-13T19:43:46  Reduce N+1 queries in reports  (rev 409)
+# 2025-11-18T21:48:03  Reduce N+1 queries in reports  (rev 414)
