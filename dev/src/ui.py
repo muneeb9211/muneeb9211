8593943b1acdbe1e33d9b04bb27202384bbef023
muneeb9211 <muneebahmad9211@gmail.com>
2025-12-12 17:35:58 +0500

    Polish loading states

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -64,3 +64,4 @@
 # 2025-11-14T10:00:44  Improve UI responsiveness  (rev 410)
 # 2025-11-25T11:17:47  Debounce search input  (rev 421)
 # 2025-12-10T20:19:13  Polish loading states  (rev 437)
+# 2025-12-12T17:35:58  Polish loading states  (rev 441)
