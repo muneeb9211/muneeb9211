cd1b41f2d3a9949d59c637a58bdf1b868a211ae4
muneeb9211 <muneebahmad9211@gmail.com>
2025-12-22 19:07:55 +0500

    Improve error messages for validation

--- dev/src/errors.py
+++ dev/src/errors.py
@@ -66,3 +66,4 @@
 # 2025-12-11T10:57:13  Improve error handling  (rev 438)
 # 2025-12-12T19:21:18  Improve error handling  (rev 442)
 # 2025-12-16T20:52:44  Improve error messages for validation  (rev 448)
+# 2025-12-22T19:07:55  Improve error messages for validation  (rev 455)
