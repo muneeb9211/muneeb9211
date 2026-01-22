ea78391522121d16ab4e2e2c47ec1b036ba80121
muneeb9211 <muneebahmad9211@gmail.com>
2026-01-22 14:49:24 +0500

    Polish loading states

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -69,3 +69,4 @@
 # 2025-12-19T20:30:16  Polish loading states  (rev 452)
 # 2026-01-05T09:15:36  Improve mobile responsiveness  (rev 463)
 # 2026-01-15T15:33:18  Polish loading states  (rev 478)
+# 2026-01-22T14:49:24  Polish loading states  (rev 486)
