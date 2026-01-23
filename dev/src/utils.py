5090fcbdcb0fd72da9d5a3d569ddcec850ceea96
muneeb9211 <muneebahmad9211@gmail.com>
2026-01-23 21:56:48 +0500

    Cleanup unused code

--- dev/src/utils.py
+++ dev/src/utils.py
@@ -69,3 +69,4 @@
 # 2026-01-12T12:34:35  Rename variables for clarity  (rev 471)
 # 2026-01-13T14:01:13  Remove dead imports  (rev 474)
 # 2026-01-22T21:31:29  Rename variables for clarity  (rev 489)
+# 2026-01-23T21:56:48  Cleanup unused code  (rev 490)
