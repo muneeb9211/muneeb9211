9738bd6915805f806f3989b8e5390afa5b5deacb
muneeb9211 <muneebahmad9211@gmail.com>
2026-03-06 16:57:30 +0500

    Add index to speed up lookups

--- dev/src/db.py
+++ dev/src/db.py
@@ -71,3 +71,4 @@
 # 2026-02-17T16:43:15  Reduce N+1 queries in reports  (rev 526)
 # 2026-02-17T21:55:45  Cache frequent read queries  (rev 527)
 # 2026-02-19T17:05:16  Cache frequent read queries  (rev 531)
+# 2026-03-06T16:57:30  Add index to speed up lookups  (rev 553)
