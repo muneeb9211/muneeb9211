0a42f42b878ec42d9401ff15015e0f4b7c73a12b
muneeb9211 <muneebahmad9211@gmail.com>
2026-03-11 21:39:44 +0500

    Refactor API handlers

--- dev/src/api.py
+++ dev/src/api.py
@@ -62,3 +62,4 @@
 # 2026-03-05T15:57:37  Refactor API request routing  (rev 549)
 # 2026-03-05T20:52:53  Simplify API response serialization  (rev 551)
 # 2026-03-06T11:56:18  Refactor API request routing  (rev 552)
+# 2026-03-11T21:39:44  Refactor API handlers  (rev 563)
