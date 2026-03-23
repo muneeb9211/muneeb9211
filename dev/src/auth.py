85cb9b606e17501a4cc1ec428da15a172fb7f39d
muneeb9211 <muneebahmad9211@gmail.com>
2026-03-23 14:56:11 +0500

    Fix authentication bug

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -51,3 +51,4 @@
 # 2026-03-08T11:02:52  Patch OAuth callback edge case  (rev 557)
 # 2026-03-18T14:32:45  Fix authentication bug  (rev 573)
 # 2026-03-22T13:15:14  Patch OAuth callback edge case  (rev 579)
+# 2026-03-23T14:56:11  Fix authentication bug  (rev 581)
