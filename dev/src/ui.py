1de62053416518b9a1be2b70c83b59c5b77a7495
muneeb9211 <muneebahmad9211@gmail.com>
2026-03-27 10:09:22 +0500

    Fix layout shift on load

--- dev/src/ui.py
+++ dev/src/ui.py
@@ -77,3 +77,4 @@
 # 2026-03-03T17:54:40  Debounce search input  (rev 545)
 # 2026-03-13T12:35:28  Polish loading states  (rev 566)
 # 2026-03-20T09:35:19  Improve UI responsiveness  (rev 577)
+# 2026-03-27T10:09:22  Fix layout shift on load  (rev 587)
