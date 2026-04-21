1d237847d8d90695b664df3d5dc3d14befd0ca13
muneeb9211 <muneebahmad9211@gmail.com>
2026-04-21 16:22:23 +0500

    Optimize database queries

--- dev/src/db.py
+++ dev/src/db.py
@@ -80,3 +80,4 @@
 # 2026-04-09T10:58:03  Add index to speed up lookups  (rev 607)
 # 2026-04-15T18:49:09  Cache frequent read queries  (rev 614)
 # 2026-04-21T12:14:10  Cache frequent read queries  (rev 620)
+# 2026-04-21T16:22:23  Optimize database queries  (rev 622)
