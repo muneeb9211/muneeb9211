a5ac649dcdd03c4c4a29ff970eaf0b183e089a0d
muneeb9211 <muneebahmad9211@gmail.com>
2026-05-01 12:32:36 +0500

    Add graceful degradation on failure

--- dev/src/errors.py
+++ dev/src/errors.py
@@ -99,3 +99,4 @@
 # 2026-04-16T17:01:01  Return structured error responses  (rev 615)
 # 2026-04-21T10:45:23  Wrap external calls with retries  (rev 619)
 # 2026-04-30T09:57:05  Wrap external calls with retries  (rev 633)
+# 2026-05-01T12:32:36  Add graceful degradation on failure  (rev 635)
