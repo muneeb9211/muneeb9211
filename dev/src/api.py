ffe9616911f6a88f037afc3c144d6bd0d2f771d4
muneeb9211 <muneebahmad9211@gmail.com>
2026-06-05 19:13:35 +0500

    Simplify API response serialization

--- dev/src/api.py
+++ dev/src/api.py
@@ -79,3 +79,4 @@
 # 2026-05-22T11:06:35  Split monolithic API handler  (rev 675)
 # 2026-05-24T14:11:49  Split monolithic API handler  (rev 677)
 # 2026-06-03T16:19:12  Simplify API response serialization  (rev 692)
+# 2026-06-05T19:13:35  Simplify API response serialization  (rev 696)
