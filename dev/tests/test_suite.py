19e0041e35d91c15bd8f1735d32c24239e0ab3dc
muneeb9211 <muneebahmad9211@gmail.com>
2026-06-22 13:23:01 +0500

    Add unit tests

--- dev/tests/test_suite.py
+++ dev/tests/test_suite.py
@@ -89,3 +89,4 @@
 # 2026-06-13T20:55:27  Add edge-case tests for parser  (rev 715)
 # 2026-06-16T17:19:25  Increase test coverage for API layer  (rev 720)
 # 2026-06-21T12:55:54  Add edge-case tests for parser  (rev 727)
+# 2026-06-22T13:23:01  Add unit tests  (rev 730)
