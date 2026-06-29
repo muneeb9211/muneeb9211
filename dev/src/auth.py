96ac216e0a351ce3b5738464492575e3fece4b59
muneeb9211 <muneebahmad9211@gmail.com>
2026-06-29 17:32:49 +0500

    Fix authentication bug in login flow

--- dev/src/auth.py
+++ dev/src/auth.py
@@ -68,3 +68,4 @@
 # 2026-05-29T16:16:53  Fix authentication bug in login flow  (rev 686)
 # 2026-06-07T19:57:16  Fix authentication bug in login flow  (rev 699)
 # 2026-06-19T20:20:09  Patch OAuth callback edge case  (rev 724)
+# 2026-06-29T17:32:49  Fix authentication bug in login flow  (rev 742)
